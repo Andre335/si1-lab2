718d2ec85d9d84f19301884dafcb4214cb569fd8
Andre335 <andre335@gmail.com>
2015-06-02 23:37:36 -0300

    tela inical finalizada + tela anuncie

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,4 +11,5 @@ public class Application extends Controller {
         return ok(index.render());
     }
 
+    public static Result anuncie() { return ok(anuncie.render()); }
 }
--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -4,6 +4,7 @@ import play.mvc.*;
 import play.test.*;
 import play.libs.F.*;
 
+import static junit.framework.Assert.*;
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
@@ -11,13 +12,21 @@ import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
+    Result result;
+
     /**
      * add your integration test here
      * in this example we just check if the welcome page is being shown
      */
     @Test
-    public void test() {
-        // só passa
+    public void testaIndex() {
+        // realiza a chamada ao método index() do Application
+        result = callAction(controllers.routes.ref.Application.index(),
+                fakeRequest());
+
+        assertThat(status(result)).isEqualTo(Http.Status.OK);
+        assertThat(contentAsString(result)).contains("Band Finder");
     }
+
 
 }
